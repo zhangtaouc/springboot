f991866483f33101fb4e412500bb3dd949ffca87
20023638 <zhangtao.xd@haier.com>
2024-03-21 09:13:42 +0800

    新增删除、编辑、跨域

--- springboot-2/src/main/java/zhangtaouc/springboot2/Mapper/StudentMapper.java
+++ springboot-2/src/main/java/zhangtaouc/springboot2/Mapper/StudentMapper.java
@@ -9,4 +9,6 @@ public interface StudentMapper {
     List<Student> selectAll();
     List<Student> getStudentByName(String name);
     int addStudent(Student student);
+    int updateStudent(Student student);
+    int deleteStudent(String name);
 }
--- springboot-2/src/main/java/zhangtaouc/springboot2/StudentController.java
+++ springboot-2/src/main/java/zhangtaouc/springboot2/StudentController.java
@@ -3,10 +3,7 @@ package zhangtaouc.springboot2;
 
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zhangtaouc.springboot2.Entity.Student;
 import zhangtaouc.springboot2.Mapper.StudentMapper;
 import zhangtaouc.springboot2.Response.Result;
@@ -15,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin("*")
 public class StudentController {
 
     @Autowired
@@ -39,8 +37,6 @@ public class StudentController {
         return JSON.toJSONString(result) ;
     }
 
-
-
     @RequestMapping(value = "/addStudent",method = RequestMethod.POST)
     public String addStudent(@RequestBody Student student) {
         System.out.println("获取到学生" + student);
@@ -56,4 +52,44 @@ public class StudentController {
         }
         return JSON.toJSONString(result) ;
     }
+
+    @RequestMapping(value = "/updateStudent",method = RequestMethod.POST)
+    public String updateStudent(@RequestBody Student student) {
+        System.out.println("获取到学生" + student);
+        int num = studentMapper.updateStudent(student);
+        if (num == 1) {
+            result.setState(200);
+            result.setRetData("更新成功");
+            result.setRetCode("00000");
+        } else {
+            result.setState(200);
+            result.setRetData("更新失败");
+            result.setRetCode("00001");
+        }
+        return JSON.toJSONString(result) ;
+    }
+
+    @RequestMapping(value = "/deleteStudent",method = RequestMethod.DELETE)
+    public String deleteStudent(String name) {
+        System.out.println("获取到的名字" + name);
+        List<Student> students = studentMapper.getStudentByName(name);
+        if (students.size() > 0) {
+            int num = studentMapper.deleteStudent(name);
+            if (num == 1) {
+                result.setState(200);
+                result.setRetData("删除成功");
+                result.setRetCode("00000");
+            } else {
+                result.setState(200);
+                result.setRetData("删除失败");
+                result.setRetCode("00001");
+            }
+        } else {
+            result.setState(200);
+            result.setRetData("删除失败，没有这个人");
+            result.setRetCode("00002");
+        }
+        return JSON.toJSONString(result) ;
+    }
+
 }
